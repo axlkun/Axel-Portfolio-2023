491109fd96f18806ba77512aabcf40e6f81bf9eb
axlkun <axelcruz.dev@gmail.com>
2023-09-09 12:35:48 -0600

    creating home section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,123 @@
 <template>
-<v-sheet>
-    <v-sheet class="home">
-
+    <v-sheet>
+        <v-sheet class="home">
+            <v-sheet style="background: transparent;">
+                <img :src="imgHome" alt="Imagen Home SVG" class="custom-sizing-img" loading="lazy" />
+            </v-sheet>
+            <v-sheet class="home-encabezado">
+                <p>Axel Cruz</p>
+                <p>Ingeniero en Sistemas ⋅ Programador</p>
+                <p> Me gusta desarrollar productos de software sólidos y escalables con una excepcional experiencia de
+                    usuario <img :src="imgHappyFace" alt="Imagen Home SVG" class="custom-sizing-img" loading="lazy" /></p>
+            </v-sheet>
+            <v-sheet class="buttons-container">
+                <a class="button">Contacto</a>
+                <a class="button">Resume / CV</a>
+            </v-sheet>
+        </v-sheet>
     </v-sheet>
-</v-sheet>
 </template>
 
 <script>
+import imgHome from '../assets/img-home.png';
+import imgHappyFace from '../assets/happyface.svg';
 
+export default {
+
+
+    data: () => ({
+        imgHome,
+        imgHappyFace
+    }),
+
+}
 </script>
 
 <style scoped>
-
-.home{
-    min-height: 100vh;
+.home {
     background-color: #f0f1fb;
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;
+
+    @media only screen and (min-width: 600px) {
+        flex-direction: column;
+    }
+}
+
+.home img{
+    max-width: 250px;
+
+}
+
+.home-encabezado {
+    /* adapta la sección de contacto*/
+    /* vista movil */
+    background: transparent;
+    max-width: 90%;
+    margin: 0 auto;
+    font-size: 16px;
+    margin-bottom: 10px;
+    
+
+    @media only screen and (min-width: 600px) {
+        font-size: 20px;
+        max-width: 60%;
+        padding: 20px 0;
+    }
+}
+
+.home-encabezado p:nth-child(1) {
+    color: #0434e4;
+    /* Color para el primer párrafo */
+}
+
+.home-encabezado p:nth-child(2) {
+    color: #757575;
+    /* Color para el segundo párrafo */
+}
+
+.home-encabezado p:nth-child(3) {
+    color: #0434e4;
+    /* Color para el segundo párrafo */
+    font-size: 35px;
+    font-weight: 400;
+    line-height: 1.2;
+
+    @media only screen and (min-width: 600px) {
+        font-size: 55px;
+    }
+}
+
+.home-encabezado img{
+    margin-bottom: -10px;
+}
+
+.buttons-container{
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    background: transparent;
+
+    @media only screen and (min-width: 600px) {
+        flex-direction: row;
+    }
+}
+
+.button{
+    background-color: #0434e4;
+    color: #f0f1fb;
+    padding: 10px 20px;
+    border-radius: 10px;
+    cursor: pointer;
+}
+
+.buttons-container a:nth-child(2){
+    background-color: #ac7ce4;
+    color: white;
+}
+
+.button:hover{
+    opacity: 0.9;
 }
 </style>
