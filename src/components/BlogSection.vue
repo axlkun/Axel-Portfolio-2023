a43b139360efded7d2e9bc6443977798c8312ebe
axlkun <axelcruz.dev@gmail.com>
2023-09-19 17:43:49 -0600

    creating the blog section

---
+++ src/components/BlogSection.vue
@@ -0,0 +1,63 @@
+<template>
+<v-sheet class="blog">
+
+    <v-sheet class="title-container">
+        <h2>Blog</h2>
+    </v-sheet>
+
+    <v-sheet class="container-blog">
+
+    </v-sheet>
+
+</v-sheet>
+</template>
+
+<script>
+export default {
+
+name: 'blogSection'
+
+}
+</script>
+
+<style scoped>
+.blog{
+    background-color: var(--primary-blue);
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.title-container {
+    font-size: 35px;
+    width: 80%;
+    margin: 0 auto;
+    color: var(--background-grey);
+    background: transparent;
+    text-align: center;
+
+    @media only screen and (min-width: 1024px) {
+        width: 90%;
+        text-align: start;
+    }
+}
+
+.title-container h2 {
+    font-weight: 400;
+
+    @media only screen and (min-width: 1024px) {
+        font-size: 55px;
+    }
+}
+
+.container-blog{
+    min-height: 80vh;
+    width: 90%;
+    background-color: var(--background-grey);
+
+    @media only screen and (min-width: 1024px) {
+        width: 60%;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,8 @@
         <homeSection></homeSection>
         <aboutMeSection></aboutMeSection>
         <servicesSection></servicesSection>
-        <WorkSection></WorkSection>
+        <workSection></workSection>
+        <blogSection></blogSection>
     </v-sheet>
 </template>
 
@@ -12,7 +13,7 @@ import homeSection from '../components/HomeSection.vue';
 import aboutMeSection from '../components/AboutMeSection.vue';
 import servicesSection from '../components/ServicesSection.vue';
 import workSection from '../components/WorkSection.vue';
-import WorkSection from '../components/WorkSection.vue';
+import blogSection from '../components/BlogSection.vue';
 
 export default {
 
@@ -20,7 +21,8 @@ export default {
     homeSection,
     aboutMeSection,
     servicesSection,
-    WorkSection
+    workSection,
+    blogSection
 }
 
 }
