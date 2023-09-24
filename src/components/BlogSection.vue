63f352c9e52a3a7ff14d7a2c086eeeba736871e1
axlkun <axelcruz.dev@gmail.com>
2023-09-24 15:38:08 -0600

    finishing articles component

--- src/components/BlogSection.vue
+++ src/components/BlogSection.vue
@@ -14,31 +14,8 @@
             </v-sheet>
 
         </v-sheet>
-        <!-- <v-sheet class="articles">
-            <v-sheet v-for="article in blogEntry" class="article-entry">
-                <v-sheet class="img-container">
-                    <v-img class="img" :src="imgBlog"></v-img>
-                    <p>{{article.date}}</p>
-                </v-sheet>
-                
-                <v-sheet>
-                    <v-sheet class="article-title">
-                        <h3>{{ article.title }}</h3>
-                        <p>{{ article.description }}</p>
-                    </v-sheet>
-
-                    <v-card-text class="d-flex flex-wrap pa-0">
-                        <v-chip v-for="tag in article.tags" class="mr-2 mb-2">
-                            {{ tag }}
-                        </v-chip>
-                    </v-card-text>
-                </v-sheet>
-
-            </v-sheet>
-
-        </v-sheet> -->
+       
         <articlestList :blogEntry="blogEntry"></articlestList>
-
 
         <v-sheet class="button-container">
             <a href="/blog" class="button">Visitar el blog <span class="mdi mdi-arrow-right-thin"></span></a>
@@ -48,7 +25,6 @@
 </template>
 
 <script>
-// import imgBlog from '../assets/logo-axel.svg';
 import articlestList from './ArticlesList.vue';
 
 
@@ -114,107 +90,6 @@ export default {
 
         font-size: 20px;
     }
-}
-
-.articles {
-    /*background-color: var(--background-grey);*/
-    padding: 30px 0;
-    width: 90%;
-    margin: 0 auto;
-    display: grid;
-    gap: 2rem;
-    background: transparent;
-    grid-template-columns: 1fr;
-    /* Establece una columna por defecto */
-
-    @media only screen and (min-width: 768px) {
-        grid-template-columns: repeat(3, 1fr);
-        /* Cambia a dos columnas en pantallas más grandes */
-        grid-row-gap: 2rem;
-        grid-column-gap: 4rem;
-    }
-}
-
-.article-entry {
-    padding-bottom: 10px;
-    border-bottom: 1px solid var(--primary-black);
-    display: flex;
-    flex-direction: column;
-    margin: 0 auto;
-    background: transparent;
-    line-height: 1.2;
-    transition: transform 0.3s, filter 0.3s;
-
-    &:hover {
-        transform: scale(1.01);
-        cursor: pointer;
-    }
-}
-
-.article-entry * {
-    background: inherit;
-}
-
-.img-container{
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-    justify-content: space-between;
-    align-items: center;
-}
-
-  .img{
-    max-width: 50px;
-  }
-
-  .article-title{
-    margin-bottom: 15px;
-  }
-
-.article-title h3 {
-    font-size: 16px;
-    text-align: start;
-    margin-bottom: 15px;
-    font-weight: bold;
-    color: var(--primary-blue);
-
-    @media only screen and (min-width: 1024px) {
-        font-size: 20px;
-    }
-}
-
-.article-entry p {
-    font-size: 16px;
-
-    @media only screen and (min-width: 1024px) {
-        font-size: 20px;
-    }
-}
-
-.article-title p {
-    color: grey;
-}
-
-.img-container p {
-    font-size: 14px;
-
-    @media only screen and (min-width: 1024px) {
-        font-size: 16px;
-    }
-}
-
-.tags {
-    display: flex;
-    flex-direction: row;
-    gap: 15px;
-    flex-wrap: wrap;
-    margin: 15px 0;
-}
-
-.tags p {
-    border: 1px solid var(--primary-black);
-    border-radius: 10px;
-    padding: 1px 5px;
 }
 
 .button-container {
