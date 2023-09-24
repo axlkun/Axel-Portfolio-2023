349fcbfdd2dc7bcffbebfa1e17b2633b517041c8
axlkun <axelcruz.dev@gmail.com>
2023-09-24 16:27:00 -0600

    creating project view

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from './views/Home.vue';
 import Work from './views/Work.vue';
 import Blog from './views/Blog.vue';
+import Project from './views/Project.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/work', component: Work },
   { path: '/blog', component: Blog },
+  { path: '/work/project', component: Project },
   
 ];
 
---
+++ src/views/Project.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-sheet class="project">
+
+        <v-sheet class="container">
+            <v-sheet class="title">
+                <h1>Unfollowers Tracker</h1>
+            </v-sheet>
+        </v-sheet>
+
+        <contactSection></contactSection>
+    </v-sheet>
+</template>
+
+<script>
+import contactSection from '../components/ContactSection.vue';
+
+export default {
+
+components: {
+    contactSection
+}
+
+}
+</script>
+
+<style scoped>
+.project{
+    background-color: var(--background-grey);
+}
+
+.container {
+    max-width: 90%;
+    background: transparent;
+    margin: 0 auto;
+}
+
+.title {
+    font-size: 35px;
+    font-weight: 400;
+    margin: 0 auto;
+    background: transparent;
+    color: var(--primary-blue);
+    text-align: center;
+
+    @media only screen and (min-width: 1024px) {
+        font-size: 55px;
+        font-weight: 400;
+        text-align: start;
+    }
+
+}
+</style>
