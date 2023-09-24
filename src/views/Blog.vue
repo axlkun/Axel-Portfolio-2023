ca1a9c83907e819153285dc4494ee406c2ed24f0
axlkun <axelcruz.dev@gmail.com>
2023-09-24 15:38:31 -0600

    using the articles component

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -24,7 +24,80 @@ export default {
         articlestList
     },
 
-    props: ['blogEntry'],
+    data: () => ({
+      blogEntry: [
+            {
+                title: "Cómo hacer un portafolio",
+                link: "/blog/como-hacer-un-portafolio",
+                description: "Conoce los puntos clave para crear un excelente portafolio web",
+                tags: ["Germany", "WellAmRhein", "Fulltime", "Other", "MidLevel"],
+                date: "10/10/2023"
+            },
+            {
+                title: "Construyendo Unfollowers Tracker",
+                link: "/blog/construyendo-unfollowers-tracker",
+                description: "Como es construir una aplicacion web para monetizar",
+                tags: ["Startup", "Adsense", "Web", "Other", "MidLevel"],
+                date: "10/10/2023"
+            },
+            {
+                title: "Proyectos para aprender Vue",
+                link: "/blog/proyectos-para-aprender-vue",
+                description: "Conoce 10 proyectos reales para prácticar tus conocimientos en Vue",
+                tags: ["Germany", "WellAmRhein", "Fulltime", "Other", "MidLevel"],
+                date: "10/10/2023"
+            },
+            {
+                title: "Hablemos sobre ChatGPT",
+                link: "/blog/hablemos-sobre-Chatgpt",
+                description: "Como sacarle el máximo provecho a esta herramienta IA",
+                tags: ["Germany", "WellAmRhein", "Fulltime", "Other", "MidLevel"],
+                date: "10/10/2023"
+            },
+            {
+                title: "¿Cual framework para frontend debo aprender?",
+                link: "/blog/cual-framework-para-frontend-debo-aprender",
+                description: "Mira esta guía para decidirte por un framework y empieza a aprender ya",
+                tags: ["Germany", "WellAmRhein", "Fulltime", "Other", "MidLevel"],
+                date: "10/10/2023"
+            },
+            {
+                title: "¿Cual framework para frontend debo aprender?",
+                link: "/blog/cual-framework-para-frontend-debo-aprender",
+                description: "Mira esta guía para decidirte por un framework y empieza a aprender ya",
+                tags: ["Germany", "WellAmRhein", "Fulltime", "Other", "MidLevel"],
+                date: "10/10/2023"
+            },
+            {
+                title: "¿Cual framework para frontend debo aprender?",
+                link: "/blog/cual-framework-para-frontend-debo-aprender",
+                description: "Mira esta guía para decidirte por un framework y empieza a aprender ya",
+                tags: ["Germany", "WellAmRhein", "Fulltime", "Other", "MidLevel"],
+                date: "10/10/2023"
+            },
+            {
+                title: "¿Cual framework para frontend debo aprender?",
+                link: "/blog/cual-framework-para-frontend-debo-aprender",
+                description: "Mira esta guía para decidirte por un framework y empieza a aprender ya",
+                tags: ["Germany", "WellAmRhein", "Fulltime", "Other", "MidLevel"],
+                date: "10/10/2023"
+            },
+            {
+                title: "¿Cual framework para frontend debo aprender?",
+                link: "/blog/cual-framework-para-frontend-debo-aprender",
+                description: "Mira esta guía para decidirte por un framework y empieza a aprender ya",
+                tags: ["Germany", "WellAmRhein", "Fulltime", "Other", "MidLevel"],
+                date: "10/10/2023"
+            },
+            {
+                title: "¿Cual framework para frontend debo aprender?",
+                link: "/blog/cual-framework-para-frontend-debo-aprender",
+                description: "Mira esta guía para decidirte por un framework y empieza a aprender ya",
+                tags: ["Germany", "WellAmRhein", "Fulltime", "Other", "MidLevel"],
+                date: "10/10/2023"
+            },
+        ]
+    }) 
 }
 
 </script>
