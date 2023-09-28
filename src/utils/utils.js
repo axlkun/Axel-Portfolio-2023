f8373f18d1b3c24088ea168e6fe1901abd37ff07
axlkun <axelcruz.dev@gmail.com>
2023-09-28 12:01:17 -0600

    new direct redirection to blog and work views

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,25 +1,33 @@
 export function scrollToSection(targetSection) {
-  const section = document.getElementById(targetSection);
-  const windowWidth = window.innerWidth;
 
-  if (section) {
-
-    if (windowWidth <= 1024) {
-
-      const headerHeight = document.querySelector('.container-bar').offsetHeight; // Obtiene la altura del encabezado
-      const sectionTop = section.getBoundingClientRect().top + headerHeight + 20;
-
-      window.scrollTo({
-        top: sectionTop,
-        behavior: 'smooth',
-      });
-    } else {
-      section.scrollIntoView({ behavior: 'smooth' });
-    }
-
-
+  if (targetSection === 'projects') {
+    window.location.href = '/work';
+  } else if (targetSection === 'blog') {
+    window.location.href = '/blog';
   } else {
-    // Si el elemento no se encuentra en la página actual, realiza una redirección a la página que contiene el elemento.
-    window.location.href = `/#${targetSection}`;
+    const section = document.getElementById(targetSection);
+    const windowWidth = window.innerWidth;
+  
+    if (section) {
+  
+      if (windowWidth <= 1024) {
+  
+        const headerHeight = document.querySelector('.container-bar').offsetHeight; // Obtiene la altura del encabezado
+        const sectionTop = section.getBoundingClientRect().top + headerHeight + 20;
+  
+        window.scrollTo({
+          top: sectionTop,
+          behavior: 'smooth',
+        });
+      } else {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
+  
+  
+    } else {
+      // Si el elemento no se encuentra en la página actual, realiza una redirección a la página que contiene el elemento.
+      window.location.href = `/#${targetSection}`;
+    }
   }
+
 }
