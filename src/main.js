86be480a761038c77ff749696c2f9ee87d31a9bd
axlkun <axelcruz.dev@gmail.com>
2023-11-07 11:38:49 -0600

    uninstall vue motion

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import router from './router.js';
-import { MotionPlugin } from '@vueuse/motion';
 
 // Vuetify
 import 'vuetify/styles';
@@ -38,5 +37,4 @@ const vuetify = createVuetify({
 const app = createApp(App)
 app.use(router)
 app.use(vuetify)
-app.use(MotionPlugin)
 app.mount('#app')
