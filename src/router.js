8e67a690e3c0fb864e04ff435a8a04ed1b1cc251
axlkun <axelcruz.dev@gmail.com>
2023-11-06 23:04:50 -0600

    remove commented code

--- src/router.js
+++ src/router.js
@@ -13,24 +13,6 @@ const routes = [
   { path: '/work/:slug',name: 'project', component: Project, props: true, },
 ];
 
-// const routes = [
-//   { path: '/', component: () => import('./views/Home.vue') },
-//   { path: '/work', component: () => import('./views/Work.vue') },
-//   { path: '/blog', component: () => import('./views/Blog.vue') },
-//   {
-//     path: '/blog/:slug',
-//     name: 'articleDetail',
-//     component: () => import('./views/Article.vue'),
-//     props: true,
-//   },
-//   {
-//     path: '/work/:slug',
-//     name: 'project',
-//     component: () => import('./views/Project.vue'),
-//     props: true,
-//   },
-// ];
-
 const router = createRouter({
   history: createWebHistory(),
   routes,
