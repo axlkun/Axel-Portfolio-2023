3e2f54a9486ff110f63460224bb4384d7fb51dcb
axlkun <axelcruz.dev@gmail.com>
2023-11-09 23:02:11 -0600

    add headers

--- src/api.js
+++ src/api.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-export default axios.create({
+const axiosInstance = axios.create({
   // baseURL: 'http://127.0.0.1:8000',
   baseURL: 'https://portfoliocms.infinityfreeapp.com',
 });
+
+// Agrega la cabecera deseada a todas las solicitudes
+axiosInstance.defaults.headers.common['Cookie'] = '__test=fe4b67cb0dbf319f4280e1c2ce166e74; expires=2024-12-13T23:22:50.381Z; path=/';
+
+export default axiosInstance;
