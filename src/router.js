574f89984f4a404ff8e3a2ae1ffd186546a0832b
axlkun <axelcruz.dev@gmail.com>
2023-11-14 22:24:29 -0600

    add dinamic title

--- src/router.js
+++ src/router.js
@@ -6,16 +6,61 @@ import Project from './views/Project.vue';
 import Article from './views/Article.vue';
 
 const routes = [
-  { path: '/', name: 'home', component: Home },
-  { path: '/work', name: 'work', component: Work },
-  { path: '/blog', name: 'blog', component: Blog },
-  { path: '/blog/:slug', name: 'articleDetail', component: Article, props: true, },
-  { path: '/work/:slug', name: 'project', component: Project, props: true, },
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+    meta: {
+      title: 'Axel Cruz | Fullstack Developer'
+    }
+  },
+
+  {
+    path: '/proyectos',
+    name: 'work',
+    component: Work,
+    meta: {
+      title: 'Axel Cruz | Proyectos'
+    }
+  },
+
+  {
+    path: '/blog',
+    name: 'blog',
+    component: Blog,
+    meta: {
+      title: 'Axel Cruz | Blog'
+    }
+  },
+
+  { 
+    path: '/blog/:slug', 
+    name: 'articleDetail', 
+    component: Article, 
+    props: true, 
+    meta: {
+      title: 'Axel Cruz | Artículo'
+    }
+  },
+
+  { 
+    path: '/proyectos/:slug', 
+    name: 'project', 
+    component: Project, 
+    props: true, 
+    meta: {
+      title: 'Axel Cruz | Proyecto'
+    }
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from) => {
+  document.title = to.meta?.title ?? 'Axel Cruz | Fullstack Developer'
 });
 
 export default router;
